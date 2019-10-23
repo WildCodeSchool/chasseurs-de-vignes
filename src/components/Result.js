af569114ab80b7a4cd7584c4dbeae5e59e447ec9
Alex' Le Coq <alexandre.lcoq@gmail.com>
2019-10-23 14:52:32 +0200

    Add Result Component

---
+++ src/components/Result.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Result.css'
+
+class Result extends React.Component {
+    render() {
+      return (
+
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+      )
+    }
+  }
+
+export default Result;
