84cd330805f69241537a63ba866a9c5b9374a259
FPaurisse <paurissefrederic@gmail.com>
2019-10-24 09:24:05 +0200

    correction results display

--- src/components/DisplayAoc.js
+++ src/components/DisplayAoc.js
@@ -6,76 +6,75 @@ class displayAoc extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 1,
+      countStart: 0,
+      total: 0,
       infos: []
     };
   }
 
   async componentDidMount() {
-    const { page } = this.state;
-    let url = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles&start=${page}&rows=${10}&facet=appellatio&facet=denominati&facet=crinao&geofilter.distance=${this.props.latitude}%2C${this.props.longitude}%2C${100000}`
+    const { countStart } = this.state;
+    let url = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles&start=${countStart}&rows=${10}&facet=appellatio&facet=denominati&facet=crinao&geofilter.distance=${
+      this.props.latitude
+    }%2C${this.props.longitude}%2C${50000}`;
     const response = await axios.get(url);
-    console.log(response)
     this.setState({ infos: response.data.records });
+    this.setState({ total: response.data.nhits });
   }
 
-  async getData(){
-    const { page } = this.state;
-    console.log(page);
-    let url = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles&start=${page}&rows=${10}&facet=appellatio&facet=denominati&facet=crinao&geofilter.distance=${this.props.latitude}%2C${this.props.longitude}%2C${100000}`
+  async getData() {
+    const { countStart } = this.state;
+    let url = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles&start=${countStart + 10}&rows=${10}&facet=appellatio&facet=denominati&facet=crinao&geofilter.distance=${
+      this.props.latitude
+    }%2C${this.props.longitude}%2C${50000}`;
     const response = await axios.get(url);
-    console.log(response)
+    console.log(response);
     this.setState({ infos: response.data.records });
+    this.setState({ total: response.data.nhits });
   }
 
-  nextPage = () => {
-        const page = this.state.page
-        this.setState({ page: page + 1 })
-        this.getData()
-    }
+  nextcountStart = () => {
+    const countStart = this.state.countStart;
+    this.setState({
+      countStart: countStart + 10
+    });
+    this.getData();
+  };
 
   render() {
-
     let myLatitude = this.props.latitude;
     let myLongitude = this.props.longitude;
     let createObjAoc = [];
     createObjAoc = this.state.infos.map(function(aoc) {
       if (aoc.fields.geo_point_2d) {
         let obj = {
-          id_app: "",
-          name_com_app: "",
-          name_app: "",
-          region: "",
-          distance: 0
+          id_app: aoc.fields.id,
+          name_com_app: aoc.fields.new_nomcom,
+          name_app: aoc.fields.denominati,
+          region: aoc.fields.crinao,
+          distance:
+            (getDistance(
+              { latitude: myLatitude, longitude: myLongitude },
+              {
+                latitude: aoc.fields.geo_point_2d[0],
+                longitude: aoc.fields.geo_point_2d[1]
+              }
+            ) / 1000).toFixed(2)
         };
-        obj.id_app = aoc.fields.id_denom;
-        obj.name_com_app = aoc.fields.new_nomcom;
-        obj.name_app = aoc.fields.denominati;
-        obj.region = aoc.fields.crinao;
-        obj.distance = (
-          getDistance(
-            { latitude: myLatitude, longitude: myLongitude },
-            {
-              latitude: aoc.fields.geo_point_2d[0],
-              longitude: aoc.fields.geo_point_2d[1]
-            }
-          ) / 1000
-        ).toFixed(2);
         return obj;
       }
     });
+
     return (
       <div>
-        <h3>AOC (-100km) :</h3>
-        <button onClick={this.nextPage}>Page {this.state.page} ›</button>
+        <hr />
+        {this.state.countStart + 10} / {this.state.total}
+        <button onClick={this.nextcountStart}>next</button>
+        <hr />
         <ul>
-          {createObjAoc.map((aoc, i) => (
-            <li key={i + "a"}>
-              <p>
-                AOC {aoc.name_app} ({aoc.name_com_app})
-              </p>
-              <p>{aoc.region}</p>
-              <p>Distance : {aoc.distance} km</p>
+          {createObjAoc.map(aoc => (
+            <li key={aoc.id_app}>
+              AOC {aoc.name_app} ({aoc.name_com_app}) › {aoc.distance} km
             </li>
           ))}
         </ul>
