f5d438db49971f0b795c6b1bedccbcec7a1ffa9b
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-10-29 10:43:37 +0100

    change class to function and add missing line at the end

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,15 @@
 import React from "react";
 import France from "@svg-maps/france.regions";
-import {SVGMap } from "react-svg-map"; 
+import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
- 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
- 
-  render() {
-    return <SVGMap map={France} onLocationClick={event => console.log(event.target.getAttribute('name'))} />;
-  }
-}
- 
+const Map = () => (
+  <div className="Map">
+    <SVGMap
+      map={France}
+      onLocationClick={event => console.log(event.target.getAttribute("name"))}
+    />
+  </div>
+);
+
 export default Map;
