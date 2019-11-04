65f17f22cfdebc081f4716b4c915fa62d6f459e9
FPaurisse <paurissefrederic@gmail.com>
2019-11-04 15:38:52 +0100

    refactor folders  structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
 import Map from "./components/Map";
 import GeoButton from "./components/GeoButton";
 import DisplayResults from "./components/DisplayResults";
