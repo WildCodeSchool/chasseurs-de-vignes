7d8ea77b44ef79febb39136709c563805aad6020
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-11-04 17:27:38 +0100

    refactor RandomImage

--- src/components/RandomImage/RandomImage.js
+++ src/components/RandomImage/RandomImage.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './RandomImage.css';
 
-class RandomImage extends React.Component{
-    constructor(props){
-        super(props);
-        this.images = ['vineyard','vine','wine','grape']
-    }
-    render(){
-    let number1 = Math.floor(Math.random() * Math.floor(this.images.length));
-    let number2 = Math.floor(Math.random() * Math.floor(this.images.length));
-    let number3 = Math.floor(Math.random() * Math.floor(this.images.length));
-    let number4 = Math.floor(Math.random() * Math.floor(this.images.length));
-    return (
-      <div className="RandomImage">
-        <div className="RandomImage-image-wrapper">
-          <img className="RandomImage-image"
-            src={`https://source.unsplash.com/600x400/?${this.images[number1]},${this.images[number2]},${this.images[number3]},${this.images[number4]}`}
-            alt="images random"
-          />
-        </div>
+const images = ['vineyard', 'vine', 'wine', 'grape'];
+
+function RandomImage() {
+  const [one, two, three, four] = images.map(i => Math.floor(Math.random() * Math.floor(i.length)));
+  return (
+    <div className="RandomImage">
+      <div className="RandomImage-image-wrapper">
+        <img className="RandomImage-image"
+          src={`https://source.unsplash.com/600x400/?${images[one]},${images[two]},${images[three]},${images[four]}`}
+          alt="images random"
+        />
       </div>
-    );
-    }
+    </div>
+  );
 }
 
 export default RandomImage;
