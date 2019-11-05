7ece27929a89e4c0d9a961ebb29b7c24140987d5
FPaurisse <paurissefrederic@gmail.com>
2019-11-05 15:43:17 +0100

    refactor Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,15 +1,17 @@
 import React from "react";
-import LoaderGif from "./images/loader.gif"
+import LoaderGif from "./images/loader.gif";
 import "./Loader.css";
 
 function Loader() {
-    return (
-      <div className="col-12">
-        <div className="Loader">
-          <img src={LoaderGif} />
+  return (
+    <div className="col-12">
+      <div className="Loader">
+        <div className="Loader__wrapper">
+          <img className="Loader__image" src={LoaderGif} />
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default Loader;
