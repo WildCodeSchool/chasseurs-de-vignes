392654ec1d90046f150d1bd53483d49e3d25f5fc
FPaurisse <paurissefrederic@gmail.com>
2019-11-05 18:10:52 +0100

    work in progress

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -6,78 +6,21 @@ import Result from "../Result/Result";
 import Loader from "../Loader/Loader";
 
 class Results extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: "",
-      radius: 50000,
-      results: [],
-      totalResults: 0,
-      totalPages: 0,
-      currentStart: 0
-    };
-  }
-
-  async componentDidMount() {
-    this.fetchAocs(0);
-  }
-
-  async fetchAocs(updatedPageNo = "") {
-    const pageNumber = updatedPageNo ? updatedPageNo : "";
-    const { latitude, longitude } = this.props.coords;
-    const { radius } = this.state;
-    const pathUrl = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles`;
-    const paramsUrl = `${pathUrl}&rows=12&start=${pageNumber}&geofilter.distance=${latitude}%2C${longitude}%2C${radius}`;
-
-    const res = await axios.get(paramsUrl);
-    const total = res.data.nhits;
-    const totalPagesCount = this.getPagesCount(total, res.data.parameters.rows);
-    this.setState({
-      data: res.data,
-      results: res.data.records,
-      totalResults: res.data.nhits,
-      currentStart: updatedPageNo,
-      totalPages: totalPagesCount
-    });
-  }
-
-  getPagesCount = (total, denominator) => {
-    const divisible = total % denominator === 0;
-    const valueToBeAdded = divisible ? 0 : 1;
-    return Math.floor(total / denominator) + valueToBeAdded;
-  };
-
-  handlePageClick = type => {
-    const { currentStart, data, query } = this.state;
-    const updatedPageNo =
-      "prev" === type
-        ? currentStart - data.parameters.rows
-        : currentStart + data.parameters.rows;
-    this.fetchAocs(updatedPageNo, query);
-  };
-
   render() {
-    const { results, currentStart, totalResults } = this.state;
-    const { latitude, longitude } = this.props.coords;
-    const { isLoading } = this.props;
-    const showPrevLink = 1 < currentStart;
-    const showNextLink = totalResults > currentStart;
+    const {
+      results,
+      isLoading,
+      coords: { latitude, longitude }
+    } = this.props;
+
     return (
       <div className="ResultsList">
-        {isLoading && (
-          <div className="col-12">
-            <Loader />
-          </div>
-        )}
-        <div className={isLoading && `ResultsList--hidden`}>
-          <div className="col-12">
-            <PageNavigation
-              showPrevLink={showPrevLink}
-              showNextLink={showNextLink}
-              handlePrevClick={() => this.handlePageClick("prev")}
-              handleNextClick={() => this.handlePageClick("next")}
-            />
-          </div>
+        <div className="row">
+          {isLoading && (
+            <div className="col-12">
+              <Loader />
+            </div>
+          )}
           {results.length > 0 && (
             <div className="col-12">
               <div className="ResultsList__group row">
