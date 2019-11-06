aa5f626e5dfda703eb56224ed5a3eac722450c42
FPaurisse <paurissefrederic@gmail.com>
2019-11-06 16:28:52 +0100

    refactor ResultsList

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -3,12 +3,7 @@ import "./ResultsList.css";
 import Result from "../Result/Result";
 import Loader from "../Loader/Loader";
 
-function Results({
-  results,
-  isLoading,
-  coords: { latitude, longitude }
-}) {
-
+function Results({ results, isLoading, coords: { latitude, longitude } }) {
   return (
     <div className="ResultsList">
       {isLoading && (
@@ -22,7 +17,8 @@ function Results({
         {results.length > 0 && (
           <div className="ResultsList__group row">
             {results.map((result, index) => (
-              <Result key={latitude+longitude+index}
+              <Result
+                key={latitude + longitude + index}
                 latitude={latitude}
                 longitude={longitude}
                 {...result.fields}
