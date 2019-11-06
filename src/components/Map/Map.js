30da1d63a5c9eb409fdf426c19ba694af81ac1aa
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-11-06 14:21:36 +0100

    add latitude, longitude for each region

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,9 +5,70 @@ import "react-svg-map/lib/index.css";
 
 const regions = {
   ara: {
-    latitude: null,
-    longitude: null
+    latitude: 45.481437830498514,
+    longitude: 4.5141887834336085
   },
+
+  bfc: {
+    latitude: 47.21963038384999,
+    longitude: 4.748610310762349
+  },
+
+  bre: {
+    latitude: 47.89207487236879,
+    longitude: -3.6118343602272613
+  },
+
+  cvl: {
+    latitude: 47.52911811949319,
+    longitude: 1.805427692205875
+  },
+
+  cor: {
+    latitude: 42.1716153,
+    longitude: 9.1710018
+  },
+
+  ges: {
+    latitude: 48.690745,
+    longitude: 5.617216
+  },
+
+  hdf: {
+    latitude: 49.955555,
+    longitude: 2.792501
+  },
+
+  idf: {
+    latitude: 49.03646231803129,
+    longitude: 2.9219899503280455
+  },
+
+  nor: {
+    latitude: 49.1256272,
+    longitude: 0.1032153
+  },
+
+  naq: {
+    latitude: 45.2033333,
+    longitude: 0.19333333333333333
+  },
+
+  occ: {
+    latitude: 43.9266695,
+    longitude: 2.1436188
+  },
+
+  pdl: {
+    latitude: 47.4599985,
+    longitude: -0.7947931
+  },
+
+  pac: {
+    latitude: 43.73853498899408,
+    longitude: 6.388010745793065
+  }
+
 }
 
 class Map extends React.Component {
@@ -24,10 +85,8 @@ class Map extends React.Component {
   }
 
   getPositionMap = () => {
-    const { clickMap } = this.state
-    if (clickMap) {
-      
-    }
+     console.log(regions.cvl)
+    
   }
 
   getMap = event => {
@@ -35,6 +94,7 @@ class Map extends React.Component {
       clickMap: true,
       regionClick: event.target.getAttribute("id")
     });
+    this.getPositionMap()
   };
 
   render() {
