acee798229cc95def8ebaf97516da653c5d57679
Andrea Maugars <andrea.maugars@gmail.com>
2019-11-06 10:37:03 +0100

    Refactor ResultsList to function

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ResultsList.css";
 import Result from "../Result/Result";
 import Loader from "../Loader/Loader";
 
-class Results extends Component {
-  render() {
-    const {
-      results,
-      isLoading,
-      coords: { latitude, longitude }
-    } = this.props;
+function Results({
+  results,
+  isLoading,
+  coords: { latitude, longitude }
+}) {
 
-    return (
-      <div className="ResultsList">
-        {isLoading && (
-          <div className="Loader__content row">
-            <div className="col-12">
-              <Loader />
-            </div>
+  return (
+    <div className="ResultsList">
+      {isLoading && (
+        <div className="Loader__content row">
+          <div className="col-12">
+            <Loader />
+          </div>
+        </div>
+      )}
+      <div className={isLoading && `ResultsList--hidden`}>
+        {results.length > 0 && (
+          <div className="ResultsList__group row">
+            {results.map(result => (
+              <Result
+                latitude={latitude}
+                longitude={longitude}
+                {...result.fields}
+              />
+            ))}
           </div>
         )}
-        <div className={isLoading && `ResultsList--hidden`}>
-          {results.length > 0 && (
-            <div className="ResultsList__group row">
-              {results.map(result => (
-                <Result
-                  latitude={latitude}
-                  longitude={longitude}
-                  {...result.fields}
-                />
-              ))}
-            </div>
-          )}
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Results;
