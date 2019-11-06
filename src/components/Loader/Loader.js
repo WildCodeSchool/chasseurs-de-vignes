2ada9176dc596f5b53952c5c4c7df7790b7ed61f
FPaurisse <paurissefrederic@gmail.com>
2019-11-06 11:41:05 +0100

    add map functions click

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,12 @@
-import React from "react";
-import LoaderGif from "./images/loader.gif";
-import "./Loader.css";
+import React from 'react';
+import LoaderGif from './images/loader.gif';
+import './Loader.css';
 
 function Loader() {
   return (
     <div className="Loader">
       <div className="Loader__wrapper">
-        <img className="Loader__image" src={LoaderGif} />
+        <img className="Loader__image" src={LoaderGif} alt="Loader" />
       </div>
     </div>
   );
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,13 +3,47 @@ import France from "@svg-maps/france.regions";
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
 
-const Map = () => (
-  <div className="Map">
-    <SVGMap
-      map={France}
-      onLocationClick={event => console.log(event.target.getAttribute("name"))}
-    />
-  </div>
-);
+const regions = {
+  ara: {
+    latitude: null,
+    longitude: null
+  },
+}
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickMap: false,
+      regionClick: null,
+      coords: {
+        latitude: null,
+        longitude: null
+      }
+    };
+  }
+
+  getPositionMap = () => {
+    const { clickMap } = this.state
+    if (clickMap) {
+      
+    }
+  }
+
+  getMap = event => {
+    this.setState({
+      clickMap: true,
+      regionClick: event.target.getAttribute("id")
+    });
+  };
+
+  render() {
+    return (
+      <div className="Map">
+        <SVGMap map={France} onLocationClick={event => this.getMap(event)} />
+      </div>
+    );
+  }
+}
 
 export default Map;
