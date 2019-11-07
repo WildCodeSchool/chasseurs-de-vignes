c0160e96ef923aca9ac19ded0f9f4dc0a658c840
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-11-07 10:45:04 +0100

    add ponctuation

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PageNotFound() {
     return (
         <div className="PageNotFound">
-            <h1>ERROR 404, Casse toi pauv'con</h1>
+            <h1>ERROR 404, Casse toi pauv'con !</h1>
         </div>
     )
 }
