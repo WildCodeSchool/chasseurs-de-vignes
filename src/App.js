f18f412638a6bc3c3c157639a206e815e1ec83d0
Tanguy Duponchelle <tanguy.duponchelle@hotmail.fr>
2019-11-14 17:22:35 +0100

    refactor archi

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Header from "./components/Header/Header";
 import MainPage from "./components/MainPage/MainPage";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-
+import axios from "axios";
+const apiURL = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles`;
+const rows = 12;
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewMethod: true,
-      fullPage: true
+      fullPage: true,
+      aocs: [],
+      totalResults: 0,
+      totalPages: 0,
+      currentStart: 0,
+      coords: {
+        latitude: null,
+        longitude: null
+      },
+      radius: 50000,
+      isLoading: true,
+      searchMethod: null,
+      nameRegion: null
     };
   }
   static propTypes = {
@@ -18,13 +32,84 @@ class App extends Component {
     location: PropTypes.object.isRequired,
     history: PropTypes.object.isRequired
   };
-
+  setCoords = coords => {
+    this.setState(
+      {
+        coords
+      },
+      () => this.fetchAocs(0)
+    );
+  };
+  setRadius = radius => {
+    this.setState(
+      {
+        radius
+      },
+      () => this.fetchAocs(0)
+    );
+  };
   changeView = view => {
     this.setState({
       viewMethod: view
     });
   };
+  async fetchAocs(numberStart) {
+    const numberStartPage = numberStart;
+    const {
+      radius,
+      coords: { latitude, longitude }
+    } = this.state;
+    const requestURL = `${apiURL}&rows=${rows}&start=${numberStartPage}&geofilter.distance=${latitude}%2C${longitude}%2C${radius}`;
 
+    this.setState({ isLoading: true, showFunction: false });
+
+    const res = await axios.get(requestURL);
+    const { nhits, records } = res.data;
+    const totalPagesCount = this.getPagesCount(nhits, rows);
+
+    this.setState({
+      aocs: records,
+      totalResults: nhits,
+      currentStart: numberStartPage,
+      totalPages: totalPagesCount,
+      isLoading: false
+    });
+  }
+
+  setSearchMethod = type => {
+    this.setState({
+      searchMethod: type
+    });
+  };
+
+  getNameRegion = region => {
+    this.setState({
+      nameRegion: region
+    });
+  };
+
+  getPagesCount = (total, denominator) => {
+    const divisible = total % denominator === 0;
+    const valueToBeAdded = divisible ? 0 : 1;
+    return Math.floor(total / denominator) + valueToBeAdded;
+  };
+
+  handlePageClick = action => {
+    const { currentStart } = this.state;
+    action === "next"
+      ? this.fetchAocs(currentStart + rows)
+      : this.fetchAocs(currentStart - rows);
+  };
+
+  hideSearchMethod = () => {
+    const { viewMethod } = this.props;
+    const changeView = !viewMethod;
+    this.changeView(changeView);
+  };
+
+  diviseScreen = view => {
+    this.changeView(view);
+  };
   hideFull = view => {
     this.setState({
       fullPage: view
@@ -45,14 +130,26 @@ class App extends Component {
     if (this.state.fullPage === false) {
       if (pathname === "/" || pathname === "/about-us") {
         this.setState({
-          fullPage: true
+          fullPage: true,
+          coords: { latitude: null, longitude: null },
+          isLoading: true
         });
       }
     }
   }
   render() {
     const { viewMethod, fullPage } = this.state;
-
+    const {
+      radius,
+      aocs,
+      currentStart,
+      totalResults,
+      coords,
+      isLoading,
+      searchMethod,
+      nameRegion,
+      rows
+    } = this.state;
     return (
       <div className="App container-fluid">
         <Header
@@ -61,6 +158,20 @@ class App extends Component {
           viewMethod={viewMethod}
         />
         <MainPage
+          changeView={this.changeView}
+          diviseScreen={this.diviseScreen}
+          setSearchMethod={this.setSearchMethod}
+          getNameRegion={this.getNameRegion}
+          setCoords={this.setCoords}
+          rows={rows}
+          nameRegion={nameRegion}
+          radius={radius}
+          aocs={aocs}
+          currentStart={currentStart}
+          totalResults={totalResults}
+          coords={coords}
+          isLoading={isLoading}
+          searchMethod={searchMethod}
           changeView={view => this.changeView(view)}
           viewMethod={viewMethod}
           fullPage={fullPage}
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./MainPage.css";
-import axios from "axios";
+
 import PageNavigation from "../PageNavigation/PageNavigation";
 import SearchBar from "../SearchBar/SearchBar";
 import GeoButton from "../GeoButton/GeoButton";
@@ -13,103 +13,10 @@ import PageNotFound from "../PageNotFound/PageNotFound";
 import HomePage from "../HomePage/HomePage";
 import AboutUs from "../AboutUs/AboutUs";
 
-const apiURL = `https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=delimitation-parcellaire-des-aoc-viticoles`;
-const rows = 12;
-
 class MainPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      aocs: [],
-      totalResults: 0,
-      totalPages: 0,
-      currentStart: 0,
-      coords: {
-        latitude: null,
-        longitude: null
-      },
-      radius: 50000,
-      isLoading: true,
-      searchMethod: null,
-      nameRegion: null
-    };
   }
-
-  setCoords = coords => {
-    this.setState(
-      {
-        coords
-      },
-      () => this.fetchAocs(0)
-    );
-  };
-
-  setRadius = radius => {
-    this.setState(
-      {
-        radius
-      },
-      () => this.fetchAocs(0)
-    );
-  };
-
-  async fetchAocs(numberStart) {
-    const numberStartPage = numberStart;
-    const {
-      radius,
-      coords: { latitude, longitude }
-    } = this.state;
-    const requestURL = `${apiURL}&rows=${rows}&start=${numberStartPage}&geofilter.distance=${latitude}%2C${longitude}%2C${radius}`;
-
-    this.setState({ isLoading: true, showFunction: false });
-
-    const res = await axios.get(requestURL);
-    const { nhits, records } = res.data;
-    const totalPagesCount = this.getPagesCount(nhits, rows);
-
-    this.setState({
-      aocs: records,
-      totalResults: nhits,
-      currentStart: numberStartPage,
-      totalPages: totalPagesCount,
-      isLoading: false
-    });
-  }
-
-  setSearchMethod = type => {
-    this.setState({
-      searchMethod: type
-    });
-  };
-
-  getNameRegion = region => {
-    this.setState({
-      nameRegion: region
-    });
-  };
-
-  getPagesCount = (total, denominator) => {
-    const divisible = total % denominator === 0;
-    const valueToBeAdded = divisible ? 0 : 1;
-    return Math.floor(total / denominator) + valueToBeAdded;
-  };
-
-  handlePageClick = action => {
-    const { currentStart } = this.state;
-    action === "next"
-      ? this.fetchAocs(currentStart + rows)
-      : this.fetchAocs(currentStart - rows);
-  };
-
-  hideSearchMethod = () => {
-    const { viewMethod } = this.props;
-    const changeView = !viewMethod;
-    this.props.changeView(changeView);
-  };
-
-  diviseScreen = view => {
-    this.props.changeView(view);
-  };
 
   render() {
     const {
@@ -120,8 +27,9 @@ class MainPage extends Component {
       coords,
       isLoading,
       searchMethod,
-      nameRegion
-    } = this.state;
+      nameRegion,
+      rows
+    } = this.props;
     const { viewMethod, fullPage } = this.props;
 
     return (
@@ -144,9 +52,9 @@ class MainPage extends Component {
                     ratione repudiandae pariatur vel iusto est?
                   </p>
                   <SearchBar
-                    afterClick={this.setCoords}
-                    changeView={view => this.diviseScreen(view)}
-                    searchMethod={this.setSearchMethod}
+                    afterClick={this.props.setCoords}
+                    changeView={view => this.props.diviseScreen(view)}
+                    searchMethod={this.props.setSearchMethod}
                   />
                 </Route>
                 <Route path="/map">
@@ -157,10 +65,10 @@ class MainPage extends Component {
                     ratione repudiandae pariatur vel iusto est?
                   </p>
                   <Map
-                    afterClick={this.setCoords}
-                    searchMethod={this.setSearchMethod}
-                    getNameRegion={this.getNameRegion}
-                    changeView={view => this.diviseScreen(view)}
+                    afterClick={this.props.setCoords}
+                    searchMethod={this.props.setSearchMethod}
+                    getNameRegion={this.props.getNameRegion}
+                    changeView={view => this.props.diviseScreen(view)}
                   />
                 </Route>
                 <Route path="/geolocation">
@@ -171,9 +79,9 @@ class MainPage extends Component {
                     ratione repudiandae pariatur vel iusto est?
                   </p>
                   <GeoButton
-                    afterClick={this.setCoords}
-                    changeView={view => this.diviseScreen(view)}
-                    searchMethod={this.setSearchMethod}
+                    afterClick={this.props.setCoords}
+                    changeView={view => this.props.diviseScreen(view)}
+                    searchMethod={this.props.setSearchMethod}
                   />
                 </Route>
                 <Route path="/about-us">
@@ -191,7 +99,7 @@ class MainPage extends Component {
             `MainPage__right--hide`} ${!viewMethod && `MainPage__right--show`}`}
         >
           <div className="button__arrow__wrapper">
-            <button onClick={this.hideSearchMethod}></button>
+            <button onClick={this.props.hideSearchMethod}></button>
           </div>
           {isLoading && (
             <div className="Loader__content">
@@ -210,13 +118,16 @@ class MainPage extends Component {
                 nameRegion={nameRegion}
               />
               <div className="results__options">
-                <Filter changeRadius={this.setRadius} currentRadius={radius} />
+                <Filter
+                  changeRadius={this.props.setRadius}
+                  currentRadius={radius}
+                />
                 {totalResults && (
                   <PageNavigation
                     currentStart={currentStart}
                     totalResults={totalResults}
                     rows={rows}
-                    handleClick={action => this.handlePageClick(action)}
+                    handleClick={action => this.props.handlePageClick(action)}
                   />
                 )}
               </div>
